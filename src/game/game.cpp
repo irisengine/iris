01e2a448bf200c8e391b3c69248a9dee03286768
iris dev <iris.engine.dev@gmail.com>
2017-12-27 11:37:56 +0000

    Integrate window into game

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+#include "window.hpp"
+
 int main()
 {
+    eng::window w{ 800.0f, 800.0f };
+
+    for(;;)
+    {
+        w.pre_render();
+        w.post_render();
+    }
+
     return 0;
 }
 
