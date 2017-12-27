99d87e6a0a6a81831cae5b5349186d876b805d2e
iris dev <iris.engine.dev@gmail.com>
2017-12-27 12:57:04 +0000

    Integrate rendering system into game

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,17 +1,65 @@
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <vector>
 
+#include "mesh.hpp"
+#include "opengl_render_system.hpp"
 #include "window.hpp"
 
-int main()
+void go()
 {
-    eng::window w{ 800.0f, 800.0f };
+    eng::window w(800.0f, 800.0f);
+
+    eng::opengl_render_system rs{ };
+
+    auto mesh1 = std::make_shared<eng::mesh>(std::vector<float>{
+        0.0f,  0.75f,
+        0.25f, 0.25f,
+       -0.25f, 0.25f
+    }, 0xFF000000);
+
+    rs.add(mesh1);
+
+    auto mesh2 = std::make_shared<eng::mesh>(std::vector<float>{
+        0.25f,  0.25f,
+        0.5f, -0.25f,
+       0.0f, -0.25f
+    }, 0x00FF0000);
+
+    rs.add(mesh2);
+
+    auto mesh3 = std::make_shared<eng::mesh>(std::vector<float>{
+        -0.25f,  0.25f,
+        -0.5f, -0.25f,
+         0.0f, -0.25f
+    }, 0x0000FF00);
+
+    rs.add(mesh3);
 
     for(;;)
     {
         w.pre_render();
+
+        rs.render();
+
         w.post_render();
     }
+}
 
-    return 0;
+int main()
+{
+    try
+    {
+        go();
+    }
+    catch(std::runtime_error &err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "unknown exception thrown" << std::endl;
+    }
 }
 
