ff553ca5551d88a5a02e95127d465dd3c2cd5a58
iris dev <iris.engine.dev@gmail.com>
2017-12-28 10:55:46 +0000

    event_dispatcher handles mouse events

--- include/engine/event_dispatcher.hpp
+++ include/engine/event_dispatcher.hpp
@@ -3,6 +3,7 @@
 #include <functional>
 
 #include "keyboard_event.hpp"
+#include "mouse_event.hpp"
 
 namespace eng
 {
@@ -17,13 +18,21 @@ class event_dispatcher final
         /** Alias for keyboard callback function type */
         using keyboard_callback = std::function<void(const keyboard_event&)>;
 
+        /** Alias for mouse callback function type */
+        using mouse_callback = std::function<void(const mouse_event&)>;
+
         /**
          * Create a enw event_dispatcher.
          *
          * @param keyboard_callback
          *   The function to call with keyboard events.
+         *
+         * @param mouse_callback
+         *   The function to call with mouse events.
          */
-        event_dispatcher(const keyboard_callback &keybaord_callback) noexcept;
+        event_dispatcher(
+            const keyboard_callback &keybaord_callback,
+            const mouse_callback &mouse_callback) noexcept;
 
         /** Default */
         ~event_dispatcher() = default;
@@ -42,10 +51,21 @@ class event_dispatcher final
          */
         void dispatch(const keyboard_event &event) const noexcept;
 
+        /**
+         * Passes a given mouse event to the set callback.
+         *
+         * @param event
+         *   The event to dispatch.
+         */
+        void dispatch(const mouse_event &event) const noexcept;
+
     private:
 
         /** Callback to pass keyboard events to */
         keyboard_callback keyboard_callback_;
+
+        /** Callback to pass mouse events to */
+        mouse_callback mouse_callback_;
 };
 
 }
--- src/engine/event_dispatcher.cpp
+++ src/engine/event_dispatcher.cpp
@@ -1,17 +1,26 @@
 #include "event_dispatcher.hpp"
 
 #include "keyboard_event.hpp"
+#include "mouse_event.hpp"
 
 namespace eng
 {
 
-event_dispatcher::event_dispatcher(const keyboard_callback &keyboard_callback) noexcept
-    : keyboard_callback_(keyboard_callback)
+event_dispatcher::event_dispatcher(
+    const keyboard_callback &keyboard_callback,
+    const mouse_callback &mouse_callback) noexcept
+    : keyboard_callback_(keyboard_callback),
+      mouse_callback_(mouse_callback)
 { }
 
 void event_dispatcher::dispatch(const keyboard_event &event) const noexcept
 {
     keyboard_callback_(event);
+}
+
+void event_dispatcher::dispatch(const mouse_event &event) const noexcept
+{
+    mouse_callback_(event);
 }
 
 }
