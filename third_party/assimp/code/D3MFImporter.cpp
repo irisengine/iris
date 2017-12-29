7393e1b58551c58443e8b3114e0c1eaf4bfbb40a
iris dev <iris.engine.dev@gmail.com>
2017-12-29 09:42:22 +0000

    Fix bug in assimp

--- third_party/assimp/code/D3MFImporter.cpp
+++ third_party/assimp/code/D3MFImporter.cpp
@@ -227,7 +227,7 @@ private:
         aiVector3D vertex;
         vertex.x = ai_strtof(xmlReader->getAttributeValue(D3MF::XmlTag::x.c_str()), nullptr);
         vertex.y = ai_strtof(xmlReader->getAttributeValue(D3MF::XmlTag::y.c_str()), nullptr);
-        vertex.z = ai_strtof>(xmlReader->getAttributeValue(D3MF::XmlTag::z.c_str()), nullptr);
+        vertex.z = ai_strtof(xmlReader->getAttributeValue(D3MF::XmlTag::z.c_str()), nullptr);
 
         return vertex;
     }
