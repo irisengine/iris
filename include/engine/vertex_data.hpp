2772dd4d27b2c35eea1d349fb47c595f2f85ebf5
iris dev <iris.engine.dev@gmail.com>
2018-01-03 19:35:32 +0000

    Store normal in vertex_data

--- include/engine/vertex_data.hpp
+++ include/engine/vertex_data.hpp
@@ -16,14 +16,22 @@ struct vertex_data
      * @param position
      *   The position of the vertex.
      *
+     * @param normal
+     *   The normal of the vertex.
+     *
+     * @param colour
+     *   Colour of the vertex.
+     *
      * @param texture_coords
      *   Coordinates of texture.
      */
     vertex_data(
         const vector3 &position,
+        const vector3 &normal,
         const vector3 &colour,
         const vector3 &texture_coords)
         : position(position),
+          normal(normal),
           colour(colour),
           texture_coords(texture_coords)
     { }
@@ -31,6 +39,10 @@ struct vertex_data
     /** Vertex position. */
     vector3 position;
 
+    /** Vertex normal. */
+    vector3 normal;
+
+    /** Vertex colour. */
     vector3 colour;
 
     /** Texture coordinates. */
