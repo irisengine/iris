53a24969431d204694c51db7d2826832eec3dbbb
iris dev <iris.engine.dev@gmail.com>
2018-03-04 09:03:59 +0000

    Add custom parser for wavefront object files

--- src/engine/entity.cpp
+++ src/engine/entity.cpp
@@ -1,14 +1,13 @@
 #include "entity.hpp"
 
-#include <iostream>
 #include <cstdint>
+#include <cstdio>
 #include <experimental/filesystem>
-#include <queue>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <string>
 #include <vector>
-
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
 
 #include "matrix4.hpp"
 #include "quaternion.hpp"
@@ -18,95 +17,146 @@ namespace
 {
 
 /**
- * Helper function to convert an assimp aiMesh object into an eng::mesh object.
- *
- * @param mesh
- *   Assimp mesh object.
+ * Helper function that reads a file into a std::string.
  *
  * @param path
- *   Path to texture file for mesh.
- *
- * @param colour
- *   Colour of meshes.
- *
- * @param position
- *   Position of mesh in world space.
- *
- * @param scale
- *   Scale of entity.
+ *   Path of file to read.
  *
  * @returns
- *   An eng::mesh object representing the supplied asismp aiMesh.
+ *   The contents of the file as a std::string.
  */
-eng::mesh create_mesh(
-    const aiMesh * const mesh,
-    const std::experimental::filesystem::path &path,
-    const eng::vector3 &colour,
-    const eng::vector3 &position,
-    const eng::vector3 &scale)
+std::string read_file(const std::experimental::filesystem::path &path)
 {
-    if(mesh == nullptr)
+    if(!std::experimental::filesystem::exists(path))
     {
-        throw std::runtime_error("mesh pointer is null");
+        throw std::runtime_error(path.string() + " does not exist");
     }
 
-    std::vector<eng::vertex_data> vertices{ };
-    std::vector<std::uint32_t> indices{ };
+    std::ifstream in{ path };
+    std::stringstream strm{ };
+    strm << in.rdbuf();
 
-    for(auto j = 0u; j < mesh->mNumVertices; ++j)
+    return strm.str();
+}
+
+/**
+ * Helper function to split a string by delimiter.
+ *
+ * @param input
+ *   String to split.
+ *
+ * @param delim
+ *   Delimiter to split at.
+ *
+ * @returns
+ *   A collection if strings, split from the input.
+ */
+std::vector<std::string> split(const std::string &input, const char delim)
+{
+    std::vector<std::string> parts{ };
+
+    std::stringstream strm{ input };
+    std::string part{ };
+
+    while(static_cast<bool>(std::getline(strm, part, delim)) != false)
     {
-        // copy vertex position
-        const auto &ai_vertex = mesh->mVertices[j];
-        eng::vector3 position{
-            ai_vertex.x,
-            ai_vertex.y,
-            ai_vertex.z };
+        parts.emplace_back(part);
+    }
 
-        // copy vertex normal
-        const auto &ai_normal = mesh->mNormals[j];
-        eng::vector3 normal{
-            ai_normal.x,
-            ai_normal.y,
-            ai_normal.z,
-        };
+    return parts;
+}
 
-        eng::vector3 texture_coords{ };
+/**
+ * Helper function to load a wavefront material file.
+ *
+ * @param parg
+ *   Path of file to load.
+ *
+ * @returns
+ *   A map of material names to diffuse colour and image files.
+ */
+std::map<std::string, std::tuple<eng::vector3, std::experimental::filesystem::path>> load_material(
+    const std::experimental::filesystem::path &path)
+{
+    const auto material_file = read_file(path);
 
-        // if mesh has vertex coords then extract them
-        if(mesh->mTextureCoords[0] != nullptr)
+    std::map<std::string, std::tuple<eng::vector3, std::experimental::filesystem::path>> materials{ };
+    eng::vector3 current_diffuse{ };
+    std::experimental::filesystem::path current_texture{ };
+    std::string current_name{ };
+
+    const auto lines = split(material_file, '\n');
+
+    // parse each line
+    for(const auto &line : lines)
+    {
+        // ignore empty lines and comments
+        if(line.empty() || (line.front() == '#'))
         {
-            texture_coords.x = mesh->mTextureCoords[0][j].x;
-            texture_coords.y = mesh->mTextureCoords[0][j].y;
+            continue;
         }
 
-        vertices.emplace_back(position, normal, colour, texture_coords);
-    }
-
-    // parse each assimp index
-    for(auto j = 0u; j < mesh->mNumFaces; ++j)
-    {
-        const auto face = mesh->mFaces[j];
-
-        for(auto k = 0u; k < face.mNumIndices; ++k)
+        // find the first space in the file, if one doesn't exist then ignore
+        const auto space_pos = line.find(' ');
+        if(space_pos == std::string::npos)
         {
-            indices.emplace_back(face.mIndices[k]);
+            continue;
+        }
+
+        // using the location of the first space separate the line into
+        // an element and tag and args
+        const auto element = line.substr(0u, space_pos);
+        const auto args = line.substr(space_pos + 1u);
+
+        if(element == "newmtl")
+        {
+            // process new material
+
+            if(!current_name.empty())
+            {
+                // we are already processing a material, so save that off
+                materials[current_name] = std::make_tuple(current_diffuse, current_texture);
+            }
+
+            // set name of current material
+            current_name = args;
+        }
+        else if(element == "Kd")
+        {
+            // process diffuse colour
+
+            auto r = 0.0f;
+            auto g = 0.0f;
+            auto b = 0.0f;
+
+            // extract colour values
+            if(std::sscanf(args.c_str(), "%f %f %f", &r, &g, &b) != 3u)
+            {
+                throw std::runtime_error("only support colour in format: v r g b");
+            }
+
+            current_diffuse = { r, g, b };
+        }
+        else if(element == "map_Kd")
+        {
+            // process diffuse image
+
+            current_texture = std::experimental::filesystem::path(path).replace_filename(args);
         }
     }
 
-    //create texture from file if it exists, else default to an all white image
-    auto texture = std::experimental::filesystem::exists(path)
-        ? eng::texture{ path }
-        : eng::texture{ { 0xFF, 0xFF, 0xFF }, 1u, 1u, 3u };
+    // save off the last material we were processing
+    materials[current_name] = std::make_tuple(current_diffuse, current_texture);
 
-    return{ vertices, indices, std::move(texture), position, scale };
+    return materials;
 }
 
 /**
  * Helper function to parse a 3d model file and create an internal engine
  * representation from it.
  *
- * Internally this method uses the assimp library so will load any format
- * it supports.
+ * This supports a subset of the wavefront object file format.
+ *
  * @param path
  *   Path to model file to load.
  *
@@ -127,72 +177,203 @@ std::vector<eng::mesh> load_file(
     const eng::vector3 &position,
     const eng::vector3 &scale)
 {
-    if(!std::experimental::filesystem::exists(path))
-    {
-        throw std::runtime_error(path.string() + " does not exist");
-    }
-
-    ::Assimp::Importer importer{ };
-
-    // parse the mesh using assimp and check it was successful
-    const auto *scene = importer.ReadFile(path.c_str(), aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-    if((scene == nullptr) ||
-       (scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE) ||
-       (scene->mRootNode == nullptr))
-    {
-        throw std::runtime_error("could not load file: " + std::string{ importer.GetErrorString() });
-    }
-
     std::vector<eng::mesh> meshes{ };
 
-    std::queue<const aiNode * const> queue;
-    queue.push(scene->mRootNode);
+    const auto file_data = read_file(path);
+    const auto lines = split(file_data, '\n');
 
-    // iterate through the assimp mesh hierarchy and convert each mesh into
-    // our own representation
-    do
+    std::map<std::string, std::tuple<eng::vector3, std::experimental::filesystem::path>> materials{ };
+    std::vector<eng::vector3> vertices{ };
+    std::vector<eng::vector3> normals{ };
+    std::vector<eng::vector3> texture_coords{ };
+    std::vector<std::uint32_t> indices{ };
+    std::vector<eng::vertex_data> vertex_data{ };
+    std::map<std::string, std::uint32_t> vertex_data_map{ };
+    std::string current_object{ };
+    std::string current_material{ };
+
+    // process each line
+    for(const auto &line : lines)
     {
-        const auto *node = queue.front();
-        queue.pop();
-
-        // convert each mesh in the current node
-        for(int i = 0u; i < node->mNumMeshes; ++i)
+        // ignore empty lines and comments
+        if(line.empty() || (line.front() == '#'))
         {
-            const auto *mesh = scene->mMeshes[node->mMeshes[i]];
-            const auto *material = scene->mMaterials[mesh->mMaterialIndex];
-            std::experimental::filesystem::path image_path{ };
+            continue;
+        }
 
-            // get diffuse texture information
-            if(material->GetTextureCount(aiTextureType_DIFFUSE) > 0)
+        // find the first space in the file, if one doesn't exist then ignore
+        const auto space_pos = line.find(' ');
+        if(space_pos == std::string::npos)
+        {
+            continue;
+        }
+
+        // using the location of the first space separate the line into
+        // an element and tag and args
+        const auto element = line.substr(0u, space_pos);
+        const auto args = line.substr(space_pos + 1u);
+
+
+        if(element == "mtllib")
+        {
+            // load material file
+            const auto material_filename = args;
+
+            const auto material = std::experimental::filesystem::path(path).replace_filename(material_filename);
+
+            materials = load_material(material);
+        }
+        else if(element == "usemtl")
+        {
+            // set current material
+
+            current_material = args;
+        }
+        else if(element == "o")
+        {
+            if(!current_object.empty())
             {
-                // get name of texture file
-                ::aiString ai_name{ };
-                material->GetTexture(aiTextureType_DIFFUSE, 0, &ai_name);
+                // we are already processing an object, so save that off
 
-                // we assume texture files are located in the same directory
-                // as the model file, so construct a new path
-                std::string filename{ ai_name.C_Str() };
-                image_path = std::experimental::filesystem::path(path).replace_filename(filename);
+                //create texture from file if it exists, else default to an all white image
+                const auto tex_path = std::get<1>(materials.at(current_material));
+                auto texture = std::experimental::filesystem::exists(tex_path)
+                    ? eng::texture{ tex_path }
+                    : eng::texture{ { 0xFF, 0xFF, 0xFF }, 1u, 1u, 3u };
+
+                meshes.emplace_back(vertex_data, indices, std::move(texture), position, scale);
+
+                // clear stored data for next object
+                vertex_data = { };
+                indices = { };
             }
 
-            aiColor3D ai_colour{ 0.0f, 0.0f, 0.0f };
-            material->Get(AI_MATKEY_COLOR_DIFFUSE, ai_colour);
-            const eng::vector3 colour{ ai_colour.r, ai_colour.g, ai_colour.b };
-
-            meshes.emplace_back(create_mesh(
-                mesh,
-                image_path,
-                colour,
-                position,
-                scale));
+            // set current object name
+            current_object = args;
         }
-
-        // queue up the child nodes for processing
-        for(int i = 0u; i < node->mNumChildren; ++i)
+        else if(element == "v")
         {
-            queue.push(node->mChildren[i]);
+            // process vertex data
+
+            auto x = 0.0f;
+            auto y = 0.0f;
+            auto z = 0.0f;
+
+            // extract vertex data
+            if(std::sscanf(args.c_str(), "%f %f %f", &x, &y, &z) != 3u)
+            {
+                throw std::runtime_error("only support vertex in format: v x y z");
+            }
+
+            vertices.emplace_back(x, y, z);
         }
-    } while(!queue.empty());
+        else if(element == "vn")
+        {
+            // process normal data
+
+            auto x = 0.0f;
+            auto y = 0.0f;
+            auto z = 0.0f;
+
+            // extract normal data
+            if(std::sscanf(args.c_str(), "%f %f %f", &x, &y, &z) != 3u)
+            {
+                throw std::runtime_error("only support normals in format: vn x y z");
+            }
+
+            normals.emplace_back(x, y, z);
+            normals.back().normalise();
+        }
+        else if(element == "vt")
+        {
+            // process texture coordinate data
+
+            auto x = 0.0f;
+            auto y = 0.0f;
+
+            // extract texture coordinate data
+            if(std::sscanf(args.c_str(), "%f %f", &x, &y) != 2u)
+            {
+                throw std::runtime_error("only support tex coords in format: vt x y");
+            }
+
+            texture_coords.emplace_back(x, y, 0.0f);
+        }
+        else if(element.substr(0, 1) == "f")
+        {
+            // process face data
+
+            // args are in the form: f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+            // so split on the space to get the separate prts
+            const auto arg_parts = split(args, ' ');
+
+            // process each part
+            for(const auto &arg : arg_parts)
+            {
+                // split again on the slash
+                const auto parts = split(arg, '/');
+
+                // only support subset of the wavefront format
+                if(parts.size() != 3u)
+                {
+                    throw std::runtime_error("only support faces with format: f v1/vt1/vn1");
+                }
+
+                // check if we have processed data for this vertex before
+                const auto index_iter = vertex_data_map.find(arg);
+
+                if(index_iter != std::cend(vertex_data_map))
+                {
+                    // if we have then just use the existing index
+                    indices.emplace_back((*index_iter).second);
+                }
+                else
+                {
+                    // this is a new vertex, so process
+
+                    // get tex coord, maybe empty so default to empty vector
+                    const auto tex_coord = parts[1].empty()
+                        ? eng::vector3{ }
+                        : texture_coords[std::stoi(parts[1]) - 1u];
+
+                    // check and get vertex position data
+                    const auto vertex_index = std::stoi(parts[0u]) - 1u;
+                    if(vertex_index > vertices.size())
+                    {
+                        throw std::runtime_error("missing vertex");
+                    }
+
+                    // check and get vertex normal data
+                    const auto normal_index = std::stoi(parts[2u]) - 1u;
+                    if(normal_index > normals.size())
+                    {
+                        throw std::runtime_error("missing normal");
+                    }
+
+                    // create vertex
+                    vertex_data.emplace_back(
+                        vertices[vertex_index],
+                        normals[normal_index],
+                        std::get<0>(materials.at(current_material)),
+                        tex_coord);
+
+                    // add this data to map so we can gets its index if we
+                    // see it again
+                    vertex_data_map[arg] = vertex_data.size() - 1u;
+                    indices.emplace_back(vertex_data.size() - 1u);
+                }
+            }
+        }
+    }
+
+    // save of the last mesh we were processing
+
+    //create texture from file if it exists, else default to an all white image
+    const auto tex_path = std::get<1>(materials.at(current_material));
+    auto texture = std::experimental::filesystem::exists(tex_path)
+        ? eng::texture{ tex_path }
+        : eng::texture{ { 0xFF, 0xFF, 0xFF }, 1u, 1u, 3u };
+    meshes.emplace_back(vertex_data, indices, std::move(texture), position, scale);
 
     return meshes;
 }
