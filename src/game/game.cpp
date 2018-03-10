d39a3f4ef34e1f69aa1e04a774c2f67699ebbb69
iris dev <iris.engine.dev@gmail.com>
2018-03-10 14:13:52 +0000

    Integrate changes into game

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -29,11 +29,6 @@ std::map<eng::key, eng::key_state> key_map;
 void keyevent_handler(const eng::keyboard_event &event)
 {
     key_map[event.key] = event.state;
-
-    if((event.key == eng::key::SPACE) && (event.state == eng::key_state::DOWN))
-    {
-        std::cout << camera->position() << std::endl;
-    }
 }
 
 void mouseevent_handler(const eng::mouse_event &event)
@@ -66,25 +61,30 @@ void go([[maybe_unused]] int argc, char **argv)
     eng::render_system rs{ camera, window, width, height };
     eng::physics_system ps{ };
 
-    rs.set_light_position({ 0.0f, 10.0f, 30.0f });
+    rs.set_light_position({ 0.0f, 50.0f, 30.0f });
 
     std::vector<std::tuple<std::shared_ptr<eng::entity>, std::shared_ptr<eng::rigid_body>>> bodies{ };
 
-    const auto add_body = [&bodies, &rs, &ps, &argv](
+    [[maybe_unused]] const auto add_body = [&bodies, &rs, &ps, &argv](
         const eng::vector3 &position,
-        const bool is_static=false)
+        const bool is_static=false,
+        const eng::vector3 &half_size=eng::vector3(1.0f, 1.0f, 1.0f),
+        const eng::quaternion &orientation=eng::quaternion())
     {
         const auto mass = 1.0f;
 
         auto entity = std::make_shared<eng::entity>(
-            std::experimental::filesystem::path{ argv[1] },
+            std::experimental::filesystem::path{ argv[4] },
             eng::vector3{ },
+            eng::quaternion{ },
             eng::vector3{ 1.0f, 1.0f, 1.0f });
         auto body = std::make_shared<eng::box>(
             position,
             mass,
+            half_size,
             is_static);
         body->set_constant_acceleration({0.0f, -10.0f, 0.0f});
+        body->warp_orientation(orientation);
 
         bodies.emplace_back(std::make_tuple(entity, body));
         rs.add(entity);
@@ -97,36 +97,115 @@ void go([[maybe_unused]] int argc, char **argv)
 
     ps.add(plane_body);
 
-    for(auto x = 0u; x < 5u; ++x)
+    for(auto i = 0u; i < 5u; ++i)
     {
-        for(auto y = 0u; y < 2u; ++y)
+        for(auto j = 0u; j < 2u; ++j)
         {
-            for(auto z = 0u; z < 5u; ++z)
+            for(auto k = 0u; k < 5u; ++k)
             {
-                add_body({x * 2.0f, 10.0f + (y * 2.0f), z * 2.0f });
+                //add_body({i * 2.0f, 10.0f + (j * 2.0f), (k * 2.0f)});
             }
         }
     }
 
-    camera->translate({0.0f, 10.0, 20.0f});
+    static const auto pi = 3.141592654f;
 
-    auto wireframe = false;
+    std::vector<std::tuple<eng::vector3, float, int>> corridors{
+        { { 0.0f, 0.0f, 0.0f }, pi / 2.0f, 1 },
+        { { 0.0f, 0.0f, -4.0f }, pi / 2.0f, 2 },
+        { { 0.0f, 0.0f, -8.0f }, pi / 2.0f, 1 },
+        { { -3.0f, 0.0f, -11.0f }, 0.0f, 1 },
+        { { -7.0f, 0.0f, -11.0f }, pi, 2 },
+        { { -11.0f, 0.0f, -11.0f }, 0.0f, 1 },
+        { { -14.0f, 0.0f, 0.0f }, pi / 2.0f, 1 },
+        { { -14.0f, 0.0f, -4.0f }, -pi / 2.0f, 2 },
+        { { -14.0f, 0.0f, -8.0f }, pi / 2.0f, 1 },
+        { { -3.0f, 0.0f, 3.0f }, 0.0f, 1 },
+        { { -7.0f, 0.0f, 3.0f }, 0.0f, 2 },
+        { { -11.0f, 0.0f, 3.0f }, 0.0f, 1 },
+        { { 0.0f, 0.0f, -11.0f }, pi, 3 },
+        { { -14.0f, 0.0f, -11.0f }, -pi / 2.0f, 3 },
+        { { 0.0f, 0.0f, 3.0f }, pi / 2.0f, 3 },
+        { { -14.0f, 0.0f, 3.0f }, 0.0f, 3 },
+    };
+
+    camera->translate({ 0.0f, 5.0f, 20.0f });
+    for(const auto &[pos, angle, index] : corridors)
+    {
+        const auto scale = 10.0f;
+        auto entity = std::make_shared<eng::entity>(
+            std::experimental::filesystem::path{ argv[index] },
+            pos * scale,
+            eng::quaternion{{ 0.0f, 1.0f, 0.0f}, angle },
+            eng::vector3{ scale, scale, scale });
+        rs.add(entity);
+        entity->set_wireframe(false);
+    }
+
+    auto camera_body = std::make_shared<eng::box>(
+        eng::vector3{ 0.0f, 3.0f, 20.0f },
+        1.0f,
+        eng::vector3{ 1.0f, 3.0f, 1.0f },
+        false);
+    camera_body->set_constant_acceleration({0.0f, -10.0f, 0.0f});
+    camera_body->set_angular_factor({0.0f, 0.0f, 0.0f});
+    camera_body->set_linear_damping(0.1f);
+    ps.add(camera_body);
+
+    auto player = std::make_shared<eng::entity>(
+        std::experimental::filesystem::path{ argv[4] },
+        eng::vector3{ },
+        eng::quaternion{ },
+        eng::vector3{ 1.0f, 3.0f, 1.0f });
+    player->set_wireframe(true);
+    rs.add(player);
+
+    std::vector<std::tuple<eng::vector3, eng::vector3>> walls {
+        { { -70.0f, 0.0f, -40.0f }, { 62.0f, 10.0f, 62.0f } },
+        { { 18.0f, 0.0f, -40.0f }, { 10.0f, 10.0f, 80.0f } },
+        { { -158.0f, 0.0f, -40.0f }, { 10.0f, 10.0f, 80.0f } },
+        { { -70.0f, 0.0f, -180.0f }, { 80.0f, 10.0f, 62.0f } },
+        { { -70.0f, 0.0f, 100.0f }, { 80.0f, 10.0f, 62.0f } },
+    };
+
+    for(const auto &[pos, half_size] : walls)
+    {
+        auto wall_body = std::make_shared<eng::box>(
+            pos,
+            1.0f,
+            half_size,
+            true);
+        ps.add(wall_body);
+
+        auto wall_entity = std::make_shared<eng::entity>(
+            std::experimental::filesystem::path{ argv[4] },
+            eng::vector3{ },
+            eng::quaternion{ },
+            half_size);
+        wall_entity->set_wireframe(true);
+        wall_entity->set_position(wall_body->position());
+        wall_entity->set_orientation(wall_body->orientation());
+        rs.add(wall_entity);
+    }
+
     auto delta = 1.0f / 30.0f;
 
     while(key_map[eng::key::Q] == eng::key_state::UP)
     {
         const auto start = std::chrono::high_resolution_clock::now();
 
-        static const float speed = 0.1f;
+        static float speed = 1.0;
 
         auto direction = camera->direction();
         direction.y = 0.0f;
 
-        auto &camera_body = std::get<1>(bodies.back());
-
         if(key_map[eng::key::SPACE] == eng::key_state::DOWN)
         {
-            camera_body->add_impulse({ 0.0f, 2.0f, 0.0f });
+            speed = 4.0f;
+        }
+        else
+        {
+            speed = 1.0f;
         }
 
         if(key_map[eng::key::W] == eng::key_state::DOWN)
@@ -149,13 +228,6 @@ void go([[maybe_unused]] int argc, char **argv)
             camera_body->add_impulse(camera->right() * speed);
         }
 
-        if(key_map[eng::key::P] == eng::key_state::DOWN)
-        {
-            wireframe = !wireframe;
-            rs.set_wireframe_mode(wireframe);
-            key_map[eng::key::P] = eng::key_state::UP;
-        }
-
         delta = 1.0f / 60.0f;
 
         ps.step(delta);
@@ -165,7 +237,8 @@ void go([[maybe_unused]] int argc, char **argv)
             const auto &[entity, body] = bodies[i];
             if(entity)
             {
-                entity->set_model(body->transform());
+                entity->set_position(body->position());
+                entity->set_orientation(body->orientation());
             }
         }
 
@@ -175,6 +248,12 @@ void go([[maybe_unused]] int argc, char **argv)
 
         const auto delta_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         delta = static_cast<float>(delta_ms.count() / 1000.0f);
+
+        player->set_position(camera_body->position());
+        player->set_orientation(camera_body->orientation());
+        camera->translate(camera_body->position() - camera->position());
+        camera->translate({0.0f, 1.0f, 0.0f});
+        rs.set_light_position(camera_body->position());
     }
 }
 
