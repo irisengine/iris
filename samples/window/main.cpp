01e4bcfcf9b2ea7124f12b9362461b0afd37f718
iris dev <iris.engine.dev@gmail.com>
2020-04-13 14:15:43 +0100

    Add window sample

---
+++ samples/window/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "log/log.hpp"
+#include "platform/event_dispatcher.hpp"
+#include "platform/window.hpp"
+
+int main()
+{
+    LOG_DEBUG("window_sample", "hello world");
+
+    bool running = true;
+
+    eng::event_dispatcher dispatcher{
+        [&running](const auto &event){ if(event.key == eng::key::Q) running = false; },
+        [](const auto&) { },
+    };
+
+    eng::window w{ dispatcher, 800, 600 };
+
+    while(running)
+    {
+        w.pre_render();
+        w.post_render();
+    }
+    LOG_ERROR("window_sample", "goodbye!");
+
+    return 0;
+}
+
