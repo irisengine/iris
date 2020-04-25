488d288e33ac835d2c7a8d8335be1a12bbcc2797
iris dev <iris.engine.dev@gmail.com>
2020-04-25 19:55:16 +0100

    Add platform utilities fos macos/ios

---
+++ src/platform/macos/macos_ios_utility.mm
@@ -0,0 +1,24 @@
+#include "platform/macos/macos_ios_utility.h"
+
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+
+namespace eng::platform::utility
+{
+
+// common utilities
+
+NSString* string_to_nsstring(const std::string &str)
+{
+    return [NSString stringWithUTF8String:str.c_str()];
+}
+
+}
+
+// platform specific implementations
+
+#if defined(PLATFORM_MACOS)
+#include "platform/macos/utility.h"
+#elif defined(PLATFORM_IOS)
+#include "platform/ios/utility.h"
+#endif
