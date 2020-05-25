abc0e166437a271e33c475eacec1a0333679934a
iris dev <iris.engine.dev@gmail.com>
2020-05-25 21:10:34 +0100

    Add real class and tests

---
+++ include/core/real.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <type_traits>
+
+namespace eng
+{
+
+/**
+ * Lightweight wrapper around a float. This provides (as much as possible) the
+ * same interface as a native float except for equality, which uses a relative
+ * epsilon comparison to account for limitations in floats.
+ */
+class real
+{
+    public:
+         
+        using type = float;
+
+        /**
+         * Construct a real from an arithmetic type. Deliberately non-explicit
+         * for a float-like API.
+         *
+         * @param value
+         *   Value of real.
+         */
+        template<class T, typename=std::enable_if_t<std::is_arithmetic_v<T>>>
+        real(T value)
+            : value(value)
+        { }
+
+        /**
+         * Cast to float.
+         */
+        operator type&()
+        {
+            return value;
+        }
+
+        /**
+         * Cast to float.
+         */
+        operator type() const
+        {
+            return value;
+        }
+
+        /** Value of real. */
+        type value;
+};
+
+// comparison operators
+// provided for comparing real vs real and real vs floating-point type
+
+inline bool operator==(real value1, real value2)
+{
+    static constexpr auto epsilon = std::numeric_limits<real::type>::epsilon();
+
+    const auto diff = std::fabs(value1 - value2);
+    value1 = std::fabs(value1);
+    value2 = std::fabs(value2);
+
+    // find largest value
+    // use an upper of one to prevent our scaled epsilon getting too large
+    const auto largest = std::max({ 1.0f, value1.value, value2.value });
+
+    // compare using a relative epsilon
+    return diff <= (largest * epsilon);
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator==(real value1, T value2)
+{
+    return value1 == real{ value2 };
+}
+
+inline bool operator!=(real value1, real value2)
+{
+    return !(value1.value == value2.value);
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator!=(real value1, T value2)
+{
+    return !(value1.value == value2);
+}
+
+inline bool operator<(real value1, real value2)
+{
+    return value1.value < value2.value;
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator<(real value1, T value2)
+{
+    return value1.value < value2;
+}
+
+inline bool operator<=(real value1, real value2)
+{
+    return (value1.value < value2.value) || (value1.value == value2.value);
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator<=(real value1, T value2)
+{
+    return (value1.value < value2) || (value1.value == value2);
+}
+
+inline bool operator>=(real value1, real value2)
+{
+    return !(value1 < value2);
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator>=(real value1, T value2)
+{
+    return !(value1 < value2);
+}
+
+inline bool operator>(real value1, real value2)
+{
+    return !(value1 <= value2);
+}
+
+template<class T, typename=std::enable_if_t<std::is_floating_point_v<T>>>
+inline bool operator>(real value1, T value2)
+{
+    return !(value1 <= value2);
+}
+
+inline std::ostream& operator<<(std::ostream &out, real r)
+{
+    out << r.value;
+    return out;
+}
+
+inline std::istream& operator>>(std::istream &in, real &r)
+{
+    in >> r.value;
+    return in;
+}
+
+}
+
---
+++ tests/core/real_tests.cpp
@@ -0,0 +1,134 @@
+#include <sstream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "core/real.h"
+
+TEST(real, constructor)
+{
+    eng::real r{ 1.1f };
+    ASSERT_EQ(r.value, 1.1f);
+}
+
+TEST(real, implicit_constructor)
+{
+    eng::real r = 1.1f;
+    ASSERT_EQ(r.value, 1.1f);
+}
+
+TEST(real, cast)
+{
+    eng::real r = 1.1f;
+    ASSERT_EQ(static_cast<float>(r), 1.1f);
+}
+
+TEST(real, const_cast)
+{
+    const eng::real r = 1.1f;
+    ASSERT_EQ(static_cast<float>(r), 1.1f);
+}
+
+TEST(real, equality)
+{
+    eng::real r1 = 0.1f;
+    eng::real r2 = 0.1f;
+
+    // create 1.0 with two different methods (addition and multiplication)
+    // these are known to produce two different values due to float limitations
+
+    for(int i = 0; i < 9; ++i)
+    {
+        r1 += 0.1f;
+    }
+
+    r2 *= 10;
+
+    // check that our values are different bit for bit, but the real class
+    // equality handles this
+    ASSERT_NE(r1.value, r2.value);
+    ASSERT_EQ(r1, r2);
+}
+
+TEST(real, inequality)
+{
+    eng::real r1 = 0.1f;
+    eng::real r2 = 0.11f;
+
+    ASSERT_NE(r1, r2);
+}
+
+TEST(real, less)
+{
+    ASSERT_LT(eng::real{ 1.0f }, eng::real{ 2.0f });
+    ASSERT_FALSE(eng::real{ 1.0f } < eng::real{ 1.0f });
+}
+
+TEST(real, less_cast)
+{
+    ASSERT_LT(eng::real{ 1.0f }, 2.0);
+    ASSERT_FALSE(eng::real{ 1.0f } < 1.0);
+}
+
+TEST(real, less_equal)
+{
+    ASSERT_LE(eng::real{ 1.0f }, eng::real{ 2.0f });
+    ASSERT_LE(eng::real{ 1.0f }, eng::real{ 1.0f });
+}
+
+TEST(real, less_equal_cast)
+{
+    ASSERT_LE(eng::real{ 1.0f }, 2.0);
+    ASSERT_LE(eng::real{ 1.0f }, 1.0);
+}
+
+TEST(real, greater)
+{
+    ASSERT_GT(eng::real{ 3.0f }, eng::real{ 2.0f });
+    ASSERT_FALSE(eng::real{ 1.0f } > eng::real{ 1.0f });
+}
+
+TEST(real, greater_cast)
+{
+    ASSERT_GT(eng::real{ 3.0f }, 2.0);
+    ASSERT_FALSE(eng::real{ 1.0f } > 1.0);
+}
+
+TEST(real, greater_equal)
+{
+    ASSERT_GE(eng::real{ 3.0f }, eng::real{ 2.0f });
+    ASSERT_GE(eng::real{ 1.0f }, eng::real{ 1.0f });
+}
+
+TEST(real, greater_equal_cast)
+{
+    ASSERT_GE(eng::real{ 3.0f }, 2.0);
+    ASSERT_GE(eng::real{ 1.0f }, 1.0);
+}
+
+TEST(real, ostream)
+{
+    std::stringstream strm1;
+    std::stringstream strm2;
+
+    eng::real v1 = 1.234f;
+    float v2 = 1.234f;
+
+    strm1 << v1;
+    strm2 << v2;
+
+    ASSERT_EQ(strm1.str(), strm2.str());
+}
+
+TEST(real, istream)
+{
+    std::string str = "1.234";
+    std::stringstream strm;
+    strm << str;
+
+    eng::real r = 0.0f;
+    strm >> r;
+
+    ASSERT_EQ(r.value, 1.234f);
+}
+
