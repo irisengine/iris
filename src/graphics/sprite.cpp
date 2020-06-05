47f3956fb5a4d8bdfc207b1cc4deb6be547e1d84
iris dev <iris.engine.dev@gmail.com>
2020-06-05 21:28:47 +0100

    Update sprite to RenderEntity

--- src/graphics/sprite.cpp
+++ src/graphics/sprite.cpp
@@ -1,77 +1,41 @@
 #include "graphics/sprite.h"
 
-#include "core/matrix4.h"
+#include "core/camera_type.h"
 #include "core/vector3.h"
 #include "graphics/material_factory.h"
 #include "graphics/shape_factory.h"
+#include "graphics/texture.h"
 #include "log/log.h"
 
 namespace eng
 {
 
 Sprite::Sprite(
-    const float x,
-    const float y,
-    const float width,
-    const float height,
+    real x,
+    real y,
+    real width,
+    real height,
     const Vector3 &colour)
     : Sprite(x, y, width, height, colour, Texture::blank())
 { }
 
 Sprite::Sprite(
-    const float x,
-    const float y,
-    const float width,
-    const float height,
+    real x,
+    real y,
+    real width,
+    real height,
     const Vector3 &colour,
     Texture &&tex)
-    : mesh_(shape_factory::sprite(colour, std::move(tex))),
-      position_(x, y, 0.0f),
-      scale_(width, height, 1.0f),
-      model_(),
-      material_(material_factory::sprite()),
-      wireframe_(false)
+    : RenderEntity(
+        shape_factory::sprite(colour, std::move(tex)),
+        { x, y, 0.0f },
+        { },
+        { width, height, 1.0f },
+        material_factory::sprite(),
+        false,
+        CameraType::ORTHOGRAPHIC)
 {
-    model_ = Matrix4::make_translate(position_) * Matrix4::make_scale(scale_);
-
-    LOG_ENGINE_INFO("entity", "constructed at: {} {}", position_, scale_);
-}
-
-void Sprite::set_position(const Vector3 &position)
-{
-    position_ = position;
-
-    model_ = Matrix4::make_translate(position_)  * Matrix4::make_scale(scale_);
-}
-
-void Sprite::set_orientation(float orientation)
-{
-    model_ = Matrix4::make_translate(position_) * Matrix4::make_rotate_z(orientation) * Matrix4::make_scale(scale_);
-}
-
-Matrix4 Sprite::transform() const
-{
-    return model_;
-}
-
-const Mesh& Sprite::mesh() const
-{
-    return mesh_;
-}
-
-const Material& Sprite::material() const
-{
-    return *material_;
-}
-
-bool Sprite::should_render_wireframe() const
-{
-    return wireframe_;
-}
-
-void Sprite::set_wireframe(const bool wireframe)
-{
-    wireframe_ = wireframe;
+    LOG_ENGINE_INFO("sprite", "constructed at: {} {}", position_, scale_);
 }
 
 }
