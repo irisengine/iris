d27062ba8b724b8962e677323c6fc38a3fb2778d
iris dev <iris.engine.dev@gmail.com>
2020-09-10 22:37:56 +0100

    Add ServerConnectionHandler class

---
+++ include/networking/server_connection_handler.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <chrono>
+#include <cstddef>
+#include <functional>
+#include <memory>
+#include <map>
+
+#include "core/data_buffer.h"
+#include "networking/accepting_socket.h"
+#include "networking/channel/channel.h"
+#include "networking/channel/channel_type.h"
+
+namespace eng
+{
+
+/**
+ * This class provides an abstraction over the low-level handling of connections.
+ * It automatically handles:
+ *  - accepting connections
+ *  - handshakes
+ *  - clock syncs
+ *  - sending/receiving data
+ * 
+ * This is all done with the lightweight Packet protocol and Channels. This
+ * class can also be provided with callbacks for key events.
+ * 
+ * Protocol:
+ * 
+ * Handshake - must be performed when client connections.
+ * 
+ * client           server
+ *          HELLO
+ *        -------->
+ * 
+ *         CONNECTED
+ *           [id]
+ *        <--------
+ * 
+ * Data - this is sent via DATA packets, ACKs may be sent in response depending
+ * on the channel used.
+ * 
+ * Sync - this allows the client to synchronise its clock with the server, always
+ * happens after handshake but my happen again if the server thinks the client
+ * is out of sync.
+ * 
+ * client           server
+ *        SYNC_START
+ *        <--------
+ * 
+ *      SYNC_RESPONSE
+ *      [client_time]
+ *        -------->
+ * 
+ *       SYNC_START
+ *      [client_time]
+ *      [server_time]
+ *        <--------
+ */
+class ServerConnectionHandler
+{
+    public:
+
+        /**
+         * Callback for new connections
+         * 
+         * @param id
+         *   A unique id for this connection.
+         */
+        using NewConnectionCallback = std::function<void(std::size_t id)>;
+
+        /**
+         * Callback for when a connection sends data.
+         * 
+         * @param id
+         *   Id of connection sending data.
+         * 
+         * @param data
+         *   The data send.
+         * 
+         * @param channel
+         *   The channel type the client sent the data on.
+         */ 
+        using RecvCallback = std::function<void(std::size_t id, const DataBuffer &data, ChannelType channel)>;
+
+        /**
+         * Create a new ServerConnectionHandler.
+         * 
+         * @param socket
+         *   The underlying socket to use.
+         * 
+         * @param new_connection
+         *   Callback to fire when a new connection is created.
+         * 
+         * @param recv
+         *   Callback to fire when data is received.
+         */
+        ServerConnectionHandler(
+            std::unique_ptr<AcceptingSocket> socket,
+            NewConnectionCallback new_connection,
+            RecvCallback recv);
+
+        // defined in implementation
+        ~ServerConnectionHandler();
+
+        // deleted
+        ServerConnectionHandler(const ServerConnectionHandler&) = delete;
+        ServerConnectionHandler& operator=(const ServerConnectionHandler&) = delete;
+
+        /**
+         * Updates the connection handler, processes all messages and fires all
+         * callbacks. This should be called regularly (e.g. from a game loop)
+         */
+        void update();
+
+        /**
+         * Send data to a connection.
+         * 
+         * @param id
+         *   Id of connection to send data to.
+         * 
+         * @param message
+         *   Data to send.
+         * 
+         * @param channel_type
+         *   The channel to send the data through
+         */
+        void send(std::size_t id, const DataBuffer &message, ChannelType channel_type);
+
+    private:
+
+        // forward declare internal struct
+        struct Connection;
+
+        /** Underlying socket. */
+        std::unique_ptr<AcceptingSocket> socket_;
+
+        /** New connection callback. */
+        NewConnectionCallback new_connection_callback_;
+
+        /** Received data callback. */
+        RecvCallback recv_callback_;
+
+        /** Start time of connection handler. */
+        std::chrono::steady_clock::time_point start_;
+
+        /** Map of connections to their unique id. */
+        std::map<std::size_t, std::unique_ptr<Connection>> connections;
+
+        /** Mutex to control access to messages. */
+        std::mutex mutex;
+
+        /** Collection of messages. */
+        std::vector<DataBuffer> messages;
+};
+
+}
---
+++ src/networking/server_connection_handler.cpp
@@ -0,0 +1,245 @@
+#include "networking/server_connection_handler.h"
+
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <vector>
+
+#include "core/data_buffer.h"
+#include "core/exception.h"
+#include "core/root.h"
+#include "jobs/concurrent_queue.h"
+#include "log/log.h"
+#include "networking/channel/channel_type.h"
+#include "networking/channel/reliable_ordered_channel.h"
+#include "networking/channel/unreliable_sequenced_channel.h"
+#include "networking/channel/unreliable_unordered_channel.h"
+#include "networking/data_buffer_deserialiser.h"
+#include "networking/data_buffer_serialiser.h"
+#include "networking/packet.h"
+#include "networking/socket.h"
+
+namespace
+{
+
+/**
+ * Helper function to handle a hello message. This is the first part of the
+ * handshake and the server needs to respond with CONNECTED. We also use this
+ * opportunity to start a sync request.
+ * 
+ * @param id
+ *   Id of connection.
+ * 
+ * @param channel
+ *   The channel HELLO was received on.
+ * 
+ * @param socket
+ *   Socket for the connection.
+ */
+void handle_hello(
+    std::size_t id,
+    eng::Channel *channel,
+    eng::Socket *socket)
+{
+    // we will send the client their id
+    eng::DataBufferSerialiser serialiser{ };
+    serialiser.push<std::uint32_t>(id);
+
+    // create and enqueue response packets
+    eng::Packet connected{ eng::PacketType::CONNECTED, eng::ChannelType::RELIABLE_ORDERED, serialiser.data() };
+    eng::Packet sync_start{ eng::PacketType::SYNC_START, eng::ChannelType::RELIABLE_ORDERED, { } };
+    channel->enqueue_send(std::move(connected));
+    channel->enqueue_send(std::move(sync_start));
+
+    // send all packets
+    for(const auto &packet : channel->yield_send_queue())
+    {
+        socket->write(packet.data(), packet.packet_size());
+    }
+}
+
+/**
+ * Helper function to handle the response to a sync.
+ * 
+ * @param channel
+ *   The channel to communicate on.
+ * 
+ * @param socket
+ *   Socket for the connection.
+ * 
+ * @param packet
+ *   The received SYNC_RESPONSE packet.
+ */
+void handle_sync_response(
+    eng::Channel *channel,
+    eng::Socket *socket,
+    const eng::Packet &packet)
+{
+    // get the client time and our time
+    eng::DataBufferDeserialiser deserialiser{ packet.body_buffer() };
+    const auto client_time_raw = deserialiser.pop<std::uint32_t>();
+    const auto now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch());
+
+    // send the client back their time and out time
+    eng::DataBufferSerialiser serialiser{ };
+    serialiser.push(client_time_raw);
+    serialiser.push<std::uint32_t>(now.count());
+    eng::Packet sync_finish{ eng::PacketType::SYNC_FINISH, eng::ChannelType::RELIABLE_ORDERED, serialiser.data() };
+    channel->enqueue_send(std::move(sync_finish));
+
+    // send all packets
+    for(const auto &p : channel->yield_send_queue())
+    {
+        socket->write(p.data(), p.packet_size());
+    }
+}
+
+}
+
+namespace eng
+{
+
+/**
+ * Helper struct to encapsulate data for a connection.
+ */
+struct ServerConnectionHandler::Connection
+{
+    Socket *socket;
+    std::map<ChannelType, std::unique_ptr<Channel>> channels;
+    std::chrono::milliseconds rtt;
+};
+
+ServerConnectionHandler::ServerConnectionHandler(
+    std::unique_ptr<AcceptingSocket> socket,
+    NewConnectionCallback new_connection_callback,
+    RecvCallback recv_callback)
+    : socket_(std::move(socket)),
+      new_connection_callback_(new_connection_callback),
+      recv_callback_(recv_callback),
+      start_(std::chrono::steady_clock::now()),
+      connections(),
+      mutex(),
+      messages()
+{
+    // we want to always be accepting connections, so we do this in a background
+    // job
+    Root::job_system().add_jobs({
+        [this]()
+        {
+            static std::size_t id = 0u;
+
+            for(;;)
+            {
+                auto *socket = socket_->accept(); 
+
+                LOG_ENGINE_INFO("server_connection_handler", "accepted connection");
+
+                if(socket != nullptr)
+                {
+                    // setup internal struct to manage connection
+                    auto connection = std::make_unique<Connection>();
+                    connection->socket = socket;
+                    connection->channels[ChannelType::UNRELIABLE_UNORDERED] = std::make_unique<UnreliableUnorderedChannel>();
+                    connection->channels[ChannelType::UNRELIABLE_SEQUENCED] = std::make_unique<UnreliableSequencedChannel>();
+                    connection->channels[ChannelType::RELIABLE_ORDERED] = std::make_unique<ReliableOrderedChannel>();
+
+                    std::unique_lock lock(mutex);
+
+                    connections[id] = std::move(connection);
+                    ++id;
+                }
+            }
+        }
+    });
+}
+
+ServerConnectionHandler::~ServerConnectionHandler() = default;
+
+void ServerConnectionHandler::update()
+{
+    std::unique_lock lock(mutex);
+
+    // process each connection
+    for(auto &[id, connection] : connections)
+    {
+        // has the connection sent us any data?
+        const auto raw_packet = connection->socket->try_read(1024);
+        if(raw_packet)
+        {
+            // convert data into a Packet
+            eng::Packet packet{ };
+            std::memcpy(packet.data(), raw_packet->data(), raw_packet->size());
+            packet.resize(raw_packet->size());
+
+            // enqueue the packet into the right channel
+            const auto channel_type = packet.channel();
+            auto *channel = connection->channels.at(channel_type).get();
+            channel->enqueue_receive(std::move(packet));
+
+            // handle all received packets from that channel
+            for(const auto &p : channel->yield_receive_queue())
+            {
+                switch(p.type())
+                {
+                    case PacketType::HELLO:
+                    {
+                        handle_hello(
+                            id,
+                            channel,
+                            connection->socket);
+
+                        // we got a new client, fire it back to the application
+                        new_connection_callback_(id);
+                        break;
+                    }
+                    case PacketType::DATA:
+                    {
+                        // we got data, fire it back to the application
+                        recv_callback_(id, p.body_buffer(), p.channel());
+                        break;
+                    }
+                    case PacketType::SYNC_RESPONSE:
+                    {
+                        handle_sync_response(
+                            channel,
+                            connection->socket,
+                            packet);
+                        break;
+                    }
+                    default:
+                        LOG_ENGINE_ERROR("server_connection_handler", "unknown packet type");
+                }
+            }
+        }
+    }
+}
+
+void ServerConnectionHandler::send(std::size_t id, const DataBuffer &message, ChannelType channel_type)
+{
+    Channel *channel = nullptr;
+    Socket *socket = nullptr;
+
+    {
+        std::unique_lock lock(mutex);
+        channel = connections[id]->channels[channel_type].get();
+        socket = connections[id]->socket;
+    }
+
+    // wrap data in a Packet and enqueue
+    Packet packet(PacketType::DATA, channel_type, message);
+    channel->enqueue_send(std::move(packet));
+
+    // send all packets
+    for(const auto &p : channel->yield_send_queue())
+    {
+       socket->write(p.data(), p.packet_size());
+    }
+}
+
+}
