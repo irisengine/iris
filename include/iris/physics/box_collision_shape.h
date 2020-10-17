2ccd3ff62848d0f9fa274af7f7e2874754daa16d
iris dev <iris.engine.dev@gmail.com>
2020-10-17 22:00:58 +0100

    Add BoxCollisionShape class

---
+++ include/iris/physics/box_collision_shape.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <any>
+#include <memory>
+
+#include "core/vector3.h"
+#include "physics/collision_shape.h"
+
+namespace iris
+{
+
+/**
+ * Implementation of CollisionShape for a box.
+ */
+class BoxCollisionShape : public CollisionShape
+{
+  public:
+    /**
+     * Construct a new BoxCollisionShape
+     *
+     * @param half_size
+     *   The extends from the center of the box which define its size.
+     */
+    BoxCollisionShape(const Vector3 &half_size);
+
+    ~BoxCollisionShape() override;
+
+    /**
+     * Get native handle for physics engine implementation of collision shape.
+     *
+     * @returns
+     *   Physics engine native handle.
+     */
+    std::any native_handle() const override;
+
+  private:
+    /** Pointer to implementation. */
+    struct implementation;
+    std::unique_ptr<implementation> impl_;
+};
+
+}
