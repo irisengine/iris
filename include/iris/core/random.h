2fd73f47834e9611a0948b846167c6ca8455a9d1
iris dev <iris.engine.dev@gmail.com>
2020-10-31 21:16:23 +0000

    Add random_element

--- include/iris/core/random.h
+++ include/iris/core/random.h
@@ -33,4 +33,36 @@ std::uint32_t random_uint32(std::uint32_t min, std::uint32_t max);
  */
 float random_float(float min, float max);
 
+/**
+ * Get a random element from a collection.
+ *
+ * @param collection
+ *   Collection to get element from.
+ *
+ * @returns
+ *   Random element.
+ */
+template <class T>
+typename T::reference random_element(T &container)
+{
+    const auto index = random_uint32(0u, container.size() - 1u);
+    return container[index];
+}
+
+/**
+ * Get a random element from a collection.
+ *
+ * @param collection
+ *   Collection to get element from.
+ *
+ * @returns
+ *   Random element.
+ */
+template <class T>
+typename T::const_reference random_element(const T &container)
+{
+    const auto index = random_uint32(0u, container.size() - 1u);
+    return container[index];
+}
+
 }
