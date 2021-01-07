d544081e80512f1715cd33cd5259cb8781499994
iris dev <iris.engine.dev@gmail.com>
2021-01-07 21:24:55 +0000

    Remove stack trace from exception

--- include/iris/core/exception.h
+++ include/iris/core/exception.h
@@ -2,13 +2,12 @@
 
 #include <stdexcept>
 #include <string>
-#include <vector>
 
 namespace iris
 {
 
 /**
- * A custom exception class that includes a stack trace.
+ * The general exception type for the engine.
  */
 class Exception : public std::runtime_error
 {
@@ -20,20 +19,6 @@ class Exception : public std::runtime_error
      *   Exception message.
      */
     Exception(const std::string &what);
-
-    /**
-     * Get a formatted copy of the stack trace. Performs C++ name
-     * de-mangling where possible.
-     *
-     * @returns
-     *   Formatted stack trace, or empty string if no stack trace was
-     *   available or it could not be formatted.
-     */
-    std::string stack_trace() const;
-
-  private:
-    /** Collection of pointers to stack frames to stack trace. */
-    std::vector<void *> frames_;
 };
 
 }
