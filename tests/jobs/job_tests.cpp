95da630e96df7e5eb394bc8a1094e66684024568
iris dev <iris.engine.dev@gmail.com>
2021-02-01 22:01:20 +0000

    Refactor tests

--- tests/jobs/job_tests.cpp
+++ tests/jobs/job_tests.cpp
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <stdexcept>
+#include <thread>
 
 #include <jobs/job_system.h>
 
@@ -7,22 +8,24 @@
 
 TEST(jobs, add_jobs_single)
 {
-    std::atomic<bool> done = false;
+    iris::JobSystem js;
+    std::atomic<int> counter = 0;
 
-    iris::JobSystem::add_jobs({[&done]() { done = true; }});
+    js.add_jobs({[&counter]() { ++counter; }});
 
-    while (!done)
+    while (counter != 1)
     {
     }
 
-    ASSERT_TRUE(done);
+    ASSERT_EQ(counter, 1);
 }
 
 TEST(jobs, add_jobs_multiple)
 {
+    iris::JobSystem js;
     std::atomic<int> counter = 0;
 
-    iris::JobSystem::add_jobs(
+    js.add_jobs(
         {[&counter]() { ++counter; },
          [&counter]() { ++counter; },
          [&counter]() { ++counter; },
@@ -37,19 +40,24 @@ TEST(jobs, add_jobs_multiple)
 
 TEST(jobs, wait_for_jobs_single)
 {
+    iris::JobSystem js;
     std::atomic<bool> done = false;
 
-    iris::JobSystem::wait_for_jobs({[&done]() { done = true; }});
+    js.wait_for_jobs({[&done]() { done = true; }});
 
     ASSERT_TRUE(done);
 }
 
 TEST(jobs, wait_for_jobs_multiple)
 {
+    iris::JobSystem js;
     std::atomic<int> counter = 0;
 
-    iris::JobSystem::wait_for_jobs(
-        {[&counter]() { ++counter; },
+    js.wait_for_jobs(
+        {[&counter]() {
+             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+             ++counter;
+         },
          [&counter]() { ++counter; },
          [&counter]() { ++counter; },
          [&counter]() { ++counter; }});
@@ -59,11 +67,12 @@ TEST(jobs, wait_for_jobs_multiple)
 
 TEST(jobs, wait_for_jobs_nested)
 {
+    iris::JobSystem js;
     std::atomic<int> counter = 0;
 
-    iris::JobSystem::wait_for_jobs({[&counter]() {
-        iris::JobSystem::wait_for_jobs({[&counter]() {
-            iris::JobSystem::wait_for_jobs({[&counter]() { ++counter; }});
+    js.wait_for_jobs({[&counter, &js]() {
+        js.wait_for_jobs({[&counter, &js]() {
+            js.wait_for_jobs({[&counter, &js]() { ++counter; }});
             ++counter;
         }});
         ++counter;
@@ -72,9 +81,47 @@ TEST(jobs, wait_for_jobs_nested)
     ASSERT_EQ(counter, 3);
 }
 
-TEST(jobs, exceptions_propogate)
+TEST(jobs, wait_for_jobs_sequential)
 {
-    const auto throws = []() { throw std::runtime_error(""); };
+    iris::JobSystem js;
+    std::atomic<int> counter = 0;
 
-    ASSERT_THROW(iris::JobSystem::wait_for_jobs({throws}), std::runtime_error);
+    js.wait_for_jobs({[&counter]() { ++counter; }});
+    js.wait_for_jobs({[&counter]() { ++counter; }});
+
+    ASSERT_EQ(counter, 2);
+}
+
+TEST(jobs, exceptions_propagate)
+{
+    iris::JobSystem js{};
+    const auto throws = []() { throw std::runtime_error(""); };
+    ASSERT_THROW(js.wait_for_jobs({throws}), std::runtime_error);
+}
+
+TEST(jobs, exceptions_propagate_complex)
+{
+    iris::JobSystem js{};
+    ASSERT_THROW(
+        js.wait_for_jobs(
+            {[&js]() {
+                 js.wait_for_jobs(
+                     {[]() {},
+                      [&js]() {
+                          js.wait_for_jobs(
+                              {[&js]() { throw std::runtime_error(""); }});
+                      },
+                      []() {},
+                      []() {}});
+             },
+             []() {}}),
+        std::runtime_error);
+}
+
+TEST(jobs, exceptions_propagate_first_job)
+{
+    iris::JobSystem js{};
+    ASSERT_THROW(
+        js.wait_for_jobs({[&js]() { throw std::runtime_error(""); }}),
+        std::runtime_error);
 }
