8b295df6198ba1b28095f1fcc783fff71c06ff42
iris dev <iris.engine.dev@gmail.com>
2021-02-01 22:07:31 +0000

    Update thread JobSystem

--- src/jobs/thread/job_system.cpp
+++ src/jobs/thread/job_system.cpp
@@ -12,27 +12,17 @@ namespace iris
 
 struct JobSystem::implementation
 {
-    std::size_t async_count;
 };
 
 JobSystem::JobSystem()
-    : impl_(std::make_unique<implementation>())
-    , running_(true)
-    , stats_stream_(nullptr)
+    : running_(true)
+    , impl_(nullptr)
 {
-    impl_->async_count = 0;
 }
 
-JobSystem::~JobSystem()
-{
-    if (stats_stream_ != nullptr)
-    {
-        *stats_stream_ << "created " << impl_->async_count << " tasks"
-                       << std::endl;
-    }
-}
+JobSystem::~JobSystem() = default;
 
-void JobSystem::add_jobs_impl(const std::vector<Job> &jobs)
+void JobSystem::add_jobs(const std::vector<Job> &jobs)
 {
     for (const auto &job : jobs)
     {
@@ -45,29 +35,22 @@ void JobSystem::add_jobs_impl(const std::vector<Job> &jobs)
         auto future = std::make_shared<std::future<void>>();
 
         *future = std::async(std::launch::async, [future, job] { job(); });
-        impl_->async_count++;
     }
 }
 
-void JobSystem::wait_for_jobs_impl(const std::vector<Job> &jobs)
+void JobSystem::wait_for_jobs(const std::vector<Job> &jobs)
 {
     std::vector<std::future<void>> waiting_jobs{};
 
     for (const auto &job : jobs)
     {
         waiting_jobs.emplace_back(std::async(std::launch::async, job));
-        impl_->async_count++;
     }
 
     for (auto &waiting_job : waiting_jobs)
     {
         waiting_job.get();
     }
-}
-
-void JobSystem::set_stats_stream_impl(std::ostream *stats_stream)
-{
-    stats_stream_ = stats_stream;
 }
 
 }
