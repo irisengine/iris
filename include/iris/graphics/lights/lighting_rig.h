90e63e2631978bb8eb008ed87cf3624f1e68b4a2
iris dev <iris.engine.dev@gmail.com>
2021-03-06 13:51:41 +0000

    Add LightingRig struct

---
+++ include/iris/graphics/lights/lighting_rig.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/colour.h"
+#include "graphics/lights/directional_light.h"
+#include "graphics/lights/point_light.h"
+
+#include <memory>
+#include <vector>
+
+namespace iris
+{
+
+/**
+ * Encapsulates all the lights for a scene.
+ */
+struct LightingRig
+{
+    /* Collection of point lights. */
+    std::vector<std::unique_ptr<PointLight>> point_lights;
+
+    /** Collection of directional lights. */
+    std::vector<std::unique_ptr<DirectionalLight>> directional_lights;
+
+    /** Ambient light colour. */
+    Colour ambient_light;
+};
+
+}
