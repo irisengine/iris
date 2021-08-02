8bfcadf32b0672fcd0fed54e978d4133455a6b50
iris dev <iris.engine.dev@gmail.com>
2021-08-02 21:14:48 +0100

    Refactor mesh_loader

--- include/iris/graphics/mesh_loader.h
+++ include/iris/graphics/mesh_loader.h
@@ -8,38 +8,33 @@
 #include "graphics/texture.h"
 #include "graphics/vertex_data.h"
 
-namespace iris
+namespace iris::mesh_loader
 {
 
 /**
- * Load a mesh from file and return requests parts.
- *
- * Note that this function does not create a Mesh object as we don't know how
- * the user wishes to construct their vertex buffers. Instead we return vertex
- * data in the engine default struct, which supports everything. It is then up
- * to the user to transform this data into their required Buffer objects (or
- * just use the returned data).
+ * Helper struct encapsulating loaded mesh data.
+ */
+struct LoadedData
+{
+    /** Mesh vertices. */
+    std::vector<VertexData> vertices;
+
+    /** Mesh indices. */
+    std::vector<std::uint32_t> indices;
+
+    /** Mesh skeleton. */
+    Skeleton skeleton;
+};
+
+/**
+ * Load a mesh from file and return its data.c
  *
  * @param mesh_name
  *   Name of of mesh to load, will be passed to ResourceLoader.
  *
- * @param vertices
- *   Optional pointer to collection of vertex data to fill.
- *
- * @param indices
- *   Optional pointer to collection of indices to fill.
- *
- * @param textures
- *   Optional pointer to collection of textures to fill.
- *
- * @param skeleton
- *   Optional pointer to skeleton to create
+ * @returns
+ *   Data loaded fro file.
  */
-void load_mesh(
-    const std::string &mesh_name,
-    std::vector<std::vector<VertexData>> *vertices,
-    std::vector<std::vector<std::uint32_t>> *indices,
-    std::vector<Texture *> *textures,
-    Skeleton *skeleton);
+LoadedData load(const std::string &mesh_name);
 
 }
