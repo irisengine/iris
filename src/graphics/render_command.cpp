5281602addc49f696f842288f92d4f6ca0318ed9
iris dev <iris.engine.dev@gmail.com>
2021-08-02 20:43:01 +0100

    Add RenderCommand class

---
+++ src/graphics/render_command.cpp
@@ -0,0 +1,113 @@
+#include "graphics/render_command.h"
+
+#include "graphics/lights/light.h"
+#include "graphics/lights/light_type.h"
+#include "graphics/material.h"
+#include "graphics/render_command_type.h"
+#include "graphics/render_entity.h"
+#include "graphics/render_pass.h"
+#include "graphics/render_target.h"
+
+namespace iris
+{
+
+RenderCommand::RenderCommand()
+    : type_(RenderCommandType::PASS_START)
+    , render_pass_(nullptr)
+    , material_(nullptr)
+    , render_entity_(nullptr)
+    , shadow_map_(nullptr)
+    , light_(nullptr)
+{
+}
+
+RenderCommand::RenderCommand(
+    RenderCommandType type,
+    const RenderPass *render_pass,
+    const Material *material,
+    const RenderEntity *render_entity,
+    const RenderTarget *shadow_map,
+    const Light *light)
+    : type_(type)
+    , render_pass_(render_pass)
+    , material_(material)
+    , render_entity_(render_entity)
+    , shadow_map_(shadow_map)
+    , light_(light)
+{
+}
+
+RenderCommandType RenderCommand::type() const
+{
+    return type_;
+}
+
+void RenderCommand::set_type(RenderCommandType type)
+{
+    type_ = type;
+}
+
+const RenderPass *RenderCommand::render_pass() const
+{
+    return render_pass_;
+}
+
+void RenderCommand::set_render_pass(const RenderPass *render_pass)
+{
+    render_pass_ = render_pass;
+}
+
+const Material *RenderCommand::material() const
+{
+    return material_;
+}
+
+void RenderCommand::set_material(const Material *material)
+{
+    material_ = material;
+}
+
+const RenderEntity *RenderCommand::render_entity() const
+{
+    return render_entity_;
+}
+
+void RenderCommand::set_render_entity(const RenderEntity *render_entity)
+{
+    render_entity_ = render_entity;
+}
+
+const Light *RenderCommand::light() const
+{
+    return light_;
+}
+
+void RenderCommand::set_light(const Light *light)
+{
+    light_ = light;
+}
+
+const RenderTarget *RenderCommand::shadow_map() const
+{
+    return shadow_map_;
+}
+
+void RenderCommand::set_shadow_map(const RenderTarget *shadow_map)
+{
+    shadow_map_ = shadow_map;
+}
+
+bool RenderCommand::operator==(const RenderCommand &other) const
+{
+    return (type_ == other.type_) && (render_pass_ == other.render_pass_) &&
+           (material_ == other.material_) &&
+           (render_entity_ == other.render_entity_) &&
+           (light_ == other.light_) && (shadow_map_ == other.shadow_map_);
+}
+
+bool RenderCommand::operator!=(const RenderCommand &other) const
+{
+    return !(*this == other);
+}
+
+}
