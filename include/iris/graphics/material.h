783b601150bbf089ec0e222eb1c584f4dadbcf29
iris dev <iris.engine.dev@gmail.com>
2021-08-02 21:42:29 +0100

    Refactor Material to an interface

--- include/iris/graphics/material.h
+++ include/iris/graphics/material.h
@@ -1,69 +1,31 @@
 #pragma once
 
-#include <any>
-#include <memory>
-#include <string>
 #include <vector>
 
-#include "graphics/lights/lighting_rig.h"
-#include "graphics/mesh.h"
-#include "graphics/render_graph/render_graph.h"
 #include "graphics/texture.h"
 
 namespace iris
 {
 
 /**
- * Class encapsulating a material for rendering an entity.
+ * Interface for a Material - a class which which encapsulates how to render
+ * a Mesh.
+ *
+ * This interface is deliberately limited, most of the functionality is provided
+ * by the implementations, which in turn is only used internally by the engine.
  */
 class Material
 {
   public:
-    /**
-     * Construct a new material.
-     *
-     * @param render_graph
-     *   RenderGraph describing material.
-     *
-     * @param mesh
-     *   Mesh.
-     *
-     * @param lighting_rig
-     *   Lights that will effect this material.
-     */
-    Material(
-        const RenderGraph *render_graph,
-        const Mesh &mesh,
-        const LightingRig *lighting_rig);
-
-    ~Material();
-    Material(Material &&);
-    Material &operator=(Material &&);
+    virtual ~Material() = default;
 
     /**
-     * Get a native handle for the material. The type of this is dependent
-     * on the current graphics API.
+     * Get all the textures used by this Material
      *
      * @returns
-     *   Graphics API specific handle.
+     *   Collection of Texture objects.
      */
-    std::any native_handle() const;
-
-    /**
-     * Get Textures used in this material.
-     *
-     * @returns
-     *   Textures used.
-     */
-    std::vector<Texture *> textures() const;
-
-  private:
-    /** Collection of textures used. */
-    std::vector<Texture *> textures_;
-
-    /** Graphics API implementation. */
-    struct implementation;
-    std::unique_ptr<implementation> impl_;
+    virtual std::vector<Texture *> textures() const = 0;
 };
 
 }
