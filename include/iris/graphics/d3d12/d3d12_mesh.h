7bc9be4605ef1b27cb228d0ecda793618da9e311
iris-engine-dev <iris.engine.dev@gmail.com>
2021-08-21 18:49:27 +0100

    Add D3D12Mesh class

---
+++ include/iris/graphics/d3d12/d3d12_mesh.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "directx/d3d12.h"
+#include "directx/d3dx12.h"
+
+#include "graphics/d3d12/d3d12_buffer.h"
+#include "graphics/mesh.h"
+#include "graphics/vertex_data.h"
+
+namespace iris
+{
+
+/**
+ * Implementation of d3d12 for metal.
+ */
+class D3D12Mesh : public Mesh
+{
+  public:
+    /**
+     * Construct a new D3D12Mesh.
+     *
+     * @param vertices
+     *   Vertices for the mesh.
+     *
+     * @param indices
+     *   Indices for the mesh.
+     *
+     * @param attributes
+     *   Attributes of the vertices.
+     */
+    D3D12Mesh(
+        const std::vector<VertexData> &vertices,
+        const std::vector<std::uint32_t> &indices,
+        const VertexAttributes &attributes);
+
+    ~D3D12Mesh() override = default;
+
+    /**
+     * Update the vertex data, this will also update any GPU data.
+     *
+     * @param data
+     *   New vertex data.
+     */
+    void update_vertex_data(const std::vector<VertexData> &data) override;
+
+    /**
+     * Update the index data, this will also update any GPU data.
+     *
+     * @param data
+     *   New index data.
+     */
+    void update_index_data(const std::vector<std::uint32_t> &data) override;
+
+    /**
+     * Get vertex buffer.
+     *
+     * @returns
+     *   Const reference to vertex buffer object.
+     */
+    const D3D12Buffer &vertex_buffer() const;
+
+    /**
+     * Get index buffer.
+     *
+     * @returns
+     *   Const reference to index buffer object.
+     */
+    const D3D12Buffer &index_buffer() const;
+
+    /**
+     * Get d3d12 object which describes vertex layout.
+     *
+     * @param
+     *   D3D12 object describing vertex.
+     */
+    std::vector<D3D12_INPUT_ELEMENT_DESC> input_descriptors() const;
+
+  private:
+    /** Buffer for vertex data. */
+    D3D12Buffer vertex_buffer_;
+
+    /** Buffer for index data. */
+    D3D12Buffer index_buffer_;
+
+    /** D3D12 object describing vertex layout. */
+    std::vector<D3D12_INPUT_ELEMENT_DESC> input_descriptors_;
+};
+
+}
---
+++ src/graphics/d3d12/d3d12_mesh.cpp
@@ -0,0 +1,105 @@
+#include "graphics/d3d12/d3d12_mesh.h"
+
+#include <memory>
+#include <tuple>
+#include <vector>
+
+#include "directx/d3d12.h"
+#include "directx/d3dx12.h"
+
+#include "core/exception.h"
+#include "graphics/vertex_attributes.h"
+
+namespace
+{
+
+/**
+ * Helper function to map engine attribute types to D3D12 types.
+ *
+ * @param type
+ *   Engine type
+ *
+ * @returns
+ *   D3D12 type.
+ */
+DXGI_FORMAT to_directx_format(iris::VertexAttributeType type)
+{
+    auto format = DXGI_FORMAT_UNKNOWN;
+
+    switch (type)
+    {
+        case iris::VertexAttributeType::FLOAT_3:
+            format = DXGI_FORMAT_R32G32B32_FLOAT;
+            break;
+        case iris::VertexAttributeType::FLOAT_4:
+            format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+            break;
+        case iris::VertexAttributeType::UINT32_1:
+            format = DXGI_FORMAT_R32_UINT;
+            break;
+        case iris::VertexAttributeType::UINT32_4:
+            format = DXGI_FORMAT_R32G32B32A32_UINT;
+            break;
+        default: throw iris::Exception("unknown vertex attribute type");
+    }
+
+    return format;
+}
+}
+
+namespace iris
+{
+
+D3D12Mesh::D3D12Mesh(
+    const std::vector<VertexData> &vertices,
+    const std::vector<std::uint32_t> &indices,
+    const VertexAttributes &attributes)
+    : vertex_buffer_(vertices)
+    , index_buffer_(indices)
+    , input_descriptors_()
+{
+    // build a D3D12 descriptors from supplied attributes
+    auto index = 0u;
+    for (const auto &[type, _1, _2, offset] : attributes)
+    {
+        // the engine doesn't care too much about vertex semantics, so we just
+        // call everything a TEXCOORD
+        input_descriptors_.push_back(
+            {"TEXCOORD",
+             index,
+             to_directx_format(type),
+             0u,
+             static_cast<UINT>(offset),
+             D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+             0u});
+
+        ++index;
+    }
+}
+
+void D3D12Mesh::update_vertex_data(const std::vector<VertexData> &data)
+{
+    vertex_buffer_.write(data);
+}
+
+void D3D12Mesh::update_index_data(const std::vector<std::uint32_t> &data)
+{
+    index_buffer_.write(data);
+}
+
+const D3D12Buffer &D3D12Mesh::vertex_buffer() const
+{
+    return vertex_buffer_;
+}
+
+const D3D12Buffer &D3D12Mesh::index_buffer() const
+{
+    return index_buffer_;
+}
+
+std::vector<D3D12_INPUT_ELEMENT_DESC> D3D12Mesh::input_descriptors() const
+{
+    return input_descriptors_;
+}
+
+}
