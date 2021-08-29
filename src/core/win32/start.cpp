82f686e0182ea663dfbf782106a7feedb53231f6
iris dev <iris.engine.dev@gmail.com>
2021-08-29 21:31:15 +0100

    Move win32 primitives

---
+++ src/core/win32/start.cpp
@@ -0,0 +1,64 @@
+#include "core/start.h"
+
+#include <memory>
+
+#include "core/root.h"
+#include "graphics/d3d12/d3d12_mesh_manager.h"
+#include "graphics/d3d12/d3d12_texture_manager.h"
+#include "graphics/opengl/opengl_mesh_manager.h"
+#include "graphics/opengl/opengl_texture_manager.h"
+#include "graphics/win32/win32_window_manager.h"
+#include "log/log.h"
+#include "log/logger.h"
+
+namespace
+{
+
+void register_apis()
+{
+    iris::Root::register_graphics_api(
+        "d3d12",
+        std::make_unique<iris::Win32WindowManager>(),
+        std::make_unique<iris::D3D12MeshManager>(),
+        std::make_unique<iris::D3D12TextureManager>());
+
+    iris::Root::register_graphics_api(
+        "opengl",
+        std::make_unique<iris::Win32WindowManager>(),
+        std::make_unique<iris::OpenGLMeshManager>(),
+        std::make_unique<iris::OpenGLTextureManager>());
+
+    iris::Root::set_graphics_api("d3d12");
+}
+
+}
+
+namespace iris
+{
+
+void start(int argc, char **argv, std::function<void(int, char **)> entry)
+{
+    PROFILE()
+
+    LOG_ENGINE_INFO("start", "engine start");
+
+    register_apis();
+
+    entry(argc, argv);
+}
+
+void start_debug(int argc, char **argv, std::function<void(int, char **)> entry)
+{
+    PROFILE()
+
+    // enable engine logging
+    Logger::instance().set_log_engine(true);
+
+    LOG_ENGINE_INFO("start", "engine start (with debugging)");
+
+    register_apis();
+
+    entry(argc, argv);
+}
+
+}
