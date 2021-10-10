d2f304d9ba823c2d642e537487ea616186c08811
iris dev <iris.engine.dev@gmail.com>
2021-10-10 22:50:56 +0100

    Remove logs in release mode

--- include/iris/log/log.h
+++ include/iris/log/log.h
@@ -9,6 +9,8 @@
 #include "log/log_level.h"
 #include "log/logger.h"
 
+#if !defined(NDEBUG)
+
 // convenient macros for logging
 #define LOG_DEBUG(T, ...)                                                      \
     iris::Logger::instance().log(                                              \
@@ -36,3 +38,19 @@
 #define LOG_ENGINE_ERROR(T, ...)                                               \
     iris::Logger::instance().log(                                              \
         iris::LogLevel::ERR, T, __FILE__, __LINE__, true, __VA_ARGS__)
+
+#else
+
+// convenient macros for logging
+#define LOG_DEBUG(T, ...)
+#define LOG_INFO(T, ...)
+#define LOG_WARN(T, ...)
+#define LOG_ERROR(T, ...)
+
+// convenient macros for engine logging
+#define LOG_ENGINE_DEBUG(T, ...)
+#define LOG_ENGINE_INFO(T, ...)
+#define LOG_ENGINE_WARN(T, ...)
+#define LOG_ENGINE_ERROR(T, ...)
+
+#endif
