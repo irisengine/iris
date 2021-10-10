1c44e9a4f28dac59da8d8b938ac42d4e56f5acbc
iris dev <iris.engine.dev@gmail.com>
2021-10-10 21:56:37 +0100

    Add copyright notice

--- src/core/random.cpp
+++ src/core/random.cpp
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+//         Distributed under the Boost Software License, Version 1.0.         //
+//            (See accompanying file LICENSE or copy at                       //
+//                 https://www.boost.org/LICENSE_1_0.txt)                     //
+////////////////////////////////////////////////////////////////////////////////
+
 #include "core/random.h"
 
 #include <random>
