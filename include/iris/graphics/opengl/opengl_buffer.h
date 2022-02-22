7fdd36a24753b607eca0016ecbb20411417f7e46
iris <iris-engine-dev@gmail.com>
2022-02-22 22:41:09 +0000

    Refactor OpenGLBuffer

--- include/iris/graphics/opengl/opengl_buffer.h
+++ include/iris/graphics/opengl/opengl_buffer.h
@@ -7,9 +7,8 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
-#include <vector>
 
+#include "core/error_handling.h"
 #include "graphics/opengl/opengl.h"
 #include "graphics/vertex_data.h"
 
@@ -17,32 +16,68 @@ namespace iris
 {
 
 /**
- * This class encapsulates an opengl buffer. A buffer can be created with either
- * vertex or index data.
+ * This class encapsulates an opengl buffer template on target and usage.
  */
+template <GLenum Target, GLenum Usage, bool FixedSize>
 class OpenGLBuffer
 {
   public:
     /**
-     * Construct a new OpenGLBuffer with vertex data.
+     * Construct a new Open GLBuffer object with an initial capacity.
      *
-     * @param vertex_data
-     *   Vertex data to copy to buffer.
+     * @param capacity
+     *   Capacity of buffer.
      */
-    OpenGLBuffer(const std::vector<VertexData> &vertex_data);
+    OpenGLBuffer(std::size_t capacity)
+        : handle_(0u)
+        , capacity_(capacity)
+    {
+        ::glGenBuffers(1, &handle_);
+        iris::expect(iris::check_opengl_error, "could not generate opengl buffer");
+
+        ::glBindBuffer(Target, handle_);
+        expect(check_opengl_error, "could not bind buffer");
+
+        ::glBufferData(Target, capacity_, nullptr, Usage);
+        expect(check_opengl_error, "could not create buffer");
+
+        ::glBindBuffer(Target, 0u);
+        expect(check_opengl_error, "could not unbind buffer");
+    }
 
     /**
-     * Construct a new OpenGLBuffer with index data.
+     * Construct a new Open GLBuffer object with an initial capacity.
      *
-     * @param vertex_data
-     *   Index data to copy to buffer.
+     * @param capacity
+     *   Capacity of buffer.
+     *
+     * @param index
+     *   Index of binding point in Target.
      */
-    OpenGLBuffer(const std::vector<std::uint32_t> &index_data);
+    OpenGLBuffer(std::size_t capacity, GLuint index)
+        : handle_(0u)
+        , capacity_(capacity)
+    {
+        ::glGenBuffers(1, &handle_);
+        iris::expect(iris::check_opengl_error, "could not generate opengl buffer");
 
-    /**
-     * Clean up opengl objects.
-     */
-    ~OpenGLBuffer();
+        ::glBindBuffer(Target, handle_);
+        expect(check_opengl_error, "could not bind buffer");
+
+        ::glBufferData(Target, capacity_, nullptr, Usage);
+        expect(check_opengl_error, "could not create buffer");
+
+        ::glBindBuffer(Target, 0u);
+        expect(check_opengl_error, "could not unbind buffer");
+
+        ::glBindBufferRange(Target, index, handle_, 0, capacity_);
+        expect(check_opengl_error, "could not set buffer range");
+    }
+
+    ~OpenGLBuffer()
+    {
+        ::glDeleteBuffers(1, &handle_);
+    }
 
     OpenGLBuffer(const OpenGLBuffer &) = delete;
     OpenGLBuffer &operator=(const OpenGLBuffer &) = delete;
@@ -53,38 +88,123 @@ class OpenGLBuffer
      * @returns
      *   OpenGL handle.
      */
-    GLuint handle() const;
+    GLuint handle() const
+    {
+        return handle_;
+    }
 
     /**
-     * Get the number of elements stored in the buffer.
+     * Get capacity of buffer.
      *
      * @returns
-     *   Number of elements in buffer.
+     *   Buffer capacity.
      */
-    std::size_t element_count() const;
+    std::size_t capacity() const
+    {
+        return capacity_;
+    }
 
     /**
-     * Write vertex data to the buffer.
+     * Write an object into the buffer at an offset.
      *
-     * @param vertex_data
-     *   New vertex data.
+     * @param object
+     *   Object to write.
+     *
+     * @param offset
+     *   Offset into buffer to write object.
      */
-    void write(const std::vector<VertexData> &vertex_data);
+    template <class T>
+    void write(const T &object, std::size_t offset)
+    {
+        write(std::addressof(object), sizeof(T), offset);
+    }
 
     /**
-     * Write index data to the buffer.
+     * Write an object into the buffer at an offset.
      *
-     * @param index_data
-     *   New index data.
+     * If offset + size > capacity then the behaviour depends on FixedSize:
+     *   true - throws
+     *   false - resizes the buffer
+     *
+     * @param object
+     *   Object to write.
+     *
+     * @param size
+     *   Size (in bytes) of object to write.
+     *
+     * @param offset
+     *   Offset into buffer to write object.
      */
-    void write(const std::vector<std::uint32_t> &index_data);
+    template <class T>
+    void write(const T *object, std::size_t size, std::size_t offset)
+    {
+        if (offset + size > capacity_)
+        {
+            if constexpr (FixedSize)
+            {
+                throw Exception("write would overflow");
+            }
+            else
+            {
+                // pick some sensible new capacity size
+                const auto old_capacity = capacity_;
+                capacity_ = std::max(offset + size, capacity_ * 2u);
+
+                // bind current handle to copy target
+                ::glBindBuffer(GL_COPY_READ_BUFFER, handle_);
+                expect(check_opengl_error, "could not bind read buffer");
+
+                // create a new handle
+                GLuint new_handle = 0u;
+                ::glGenBuffers(1u, &new_handle);
+                expect(check_opengl_error, "could not create new buffer");
+
+                // bind new handle to existing target
+                ::glBindBuffer(Target, new_handle);
+                expect(check_opengl_error, "could not bind write buffer");
+
+                // allocate space for new handle
+                ::glBufferData(Target, capacity_, nullptr, Usage);
+                expect(check_opengl_error, "could not allocate write buffer");
+
+                // copy data from old handle to new handle
+                ::glCopyBufferSubData(GL_COPY_READ_BUFFER, Target, 0, 0, old_capacity);
+                expect(check_opengl_error, "could not copy data");
+
+                // delete old handle
+                ::glDeleteBuffers(1, &handle_);
+                expect(check_opengl_error, "could not delete old handle");
+
+                // update new handle - note that if this buffer is a VBO or EBO it will invalidate any VAOs
+                handle_ = new_handle;
+
+                ::glBindBuffer(GL_COPY_READ_BUFFER, 0u);
+                expect(check_opengl_error, "could not unbind read buffer");
+            }
+        }
+
+        ::glBindBuffer(Target, handle_);
+        expect(check_opengl_error, "could not bind buffer");
+
+        ::glBufferSubData(Target, offset, size, object);
+        expect(check_opengl_error, "could not set buffer data");
+
+        ::glBindBuffer(Target, 0u);
+        expect(check_opengl_error, "could not unbind buffer");
+    }
 
   private:
     /** OpenGL handle for buffer. */
     GLuint handle_;
 
-    /** Number of elements in buffer. */
-    std::size_t element_count_;
+    /** Capacity of buffer. */
+    std::size_t capacity_;
 };
+
+// helper aliases
+using VBO = OpenGLBuffer<GL_ARRAY_BUFFER, GL_STATIC_DRAW, false>;
+using EBO = OpenGLBuffer<GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, false>;
+using UBO = OpenGLBuffer<GL_UNIFORM_BUFFER, GL_DYNAMIC_DRAW, true>;
+using SSBO = OpenGLBuffer<GL_SHADER_STORAGE_BUFFER, GL_DYNAMIC_DRAW, true>;
 
 }
--- src/graphics/opengl/opengl_mesh.cpp
+++ src/graphics/opengl/opengl_mesh.cpp
@@ -10,6 +10,7 @@
 #include <tuple>
 
 #include "core/error_handling.h"
+#include "graphics/constant_buffer_writer.h"
 #include "graphics/opengl/opengl.h"
 #include "graphics/vertex_attributes.h"
 
@@ -23,8 +24,7 @@ namespace
  *   Type to convert
  *
  * @returns
- *   Tuple of OpenGL type and boolean indicating if the returned type is a
- *  float type.
+ *   Tuple of OpenGL type and boolean indicating if the returned type is a float type.
  */
 std::tuple<GLenum, bool> to_opengl_format(iris::VertexAttributeType type)
 {
@@ -56,14 +56,76 @@ OpenGLMesh::OpenGLMesh(
     const std::vector<std::uint32_t> &indices,
     const VertexAttributes &attributes)
     : Mesh(vertices, indices)
-    , vertex_buffer_(vertices_)
-    , index_buffer_(indices_)
+    , attributes_(attributes)
+    , vertex_buffer_(vertices_.size() * sizeof(VertexData))
+    , index_buffer_(indices_.size() * sizeof(std::uint32_t))
     , vao_(0u)
+    , element_count_(0u)
 {
+    update_vertex_data(vertices_);
+    update_index_data(indices_);
+
     // create vao
     ::glGenVertexArrays(1, &vao_);
     expect(check_opengl_error, "could not generate vao");
 
+    setup_vao();
+}
+
+OpenGLMesh::~OpenGLMesh()
+{
+    ::glDeleteVertexArrays(1u, &vao_);
+}
+
+void OpenGLMesh::update_vertex_data(const std::vector<VertexData> &data)
+{
+    const auto capacity = vertex_buffer_.capacity();
+
+    ConstantBufferWriter writer{vertex_buffer_};
+    writer.write(data);
+
+    // if buffer resized then we need to re-setup the vao
+    if (capacity != vertex_buffer_.capacity())
+    {
+        setup_vao();
+    }
+}
+
+void OpenGLMesh::update_index_data(const std::vector<std::uint32_t> &data)
+{
+    const auto capacity = index_buffer_.capacity();
+
+    ConstantBufferWriter writer{index_buffer_};
+    writer.write(data);
+
+    element_count_ = static_cast<GLsizei>(data.size());
+
+    // if buffer resized then we need to re-setup the vao
+    if (capacity != index_buffer_.capacity())
+    {
+        setup_vao();
+    }
+}
+
+GLsizei OpenGLMesh::element_count() const
+{
+    return element_count_;
+}
+
+void OpenGLMesh::bind() const
+{
+    ::glBindVertexArray(vao_);
+    expect(check_opengl_error, "could not bind vao");
+}
+
+void OpenGLMesh::unbind() const
+{
+    ::glBindVertexArray(0u);
+    expect(check_opengl_error, "could not unbind vao");
+}
+
+void OpenGLMesh::setup_vao()
+{
     bind();
 
     // ensure both buffers are bound for the vao
@@ -75,7 +137,7 @@ OpenGLMesh::OpenGLMesh(
     auto index = 0u;
 
     // define the vertex attribute data for opengl
-    for (const auto &attribute : attributes)
+    for (const auto &attribute : attributes_)
     {
         const auto &[type, components, _, offset] = attribute;
 
@@ -91,7 +153,7 @@ OpenGLMesh::OpenGLMesh(
                 static_cast<GLint>(components),
                 open_gl_type,
                 GL_FALSE,
-                static_cast<GLsizei>(attributes.size()),
+                static_cast<GLsizei>(attributes_.size()),
                 reinterpret_cast<void *>(offset));
             expect(check_opengl_error, "could not set attributes");
         }
@@ -101,7 +163,7 @@ OpenGLMesh::OpenGLMesh(
                 index,
                 static_cast<GLint>(components),
                 open_gl_type,
-                static_cast<GLsizei>(attributes.size()),
+                static_cast<GLsizei>(attributes_.size()),
                 reinterpret_cast<void *>(offset));
             expect(check_opengl_error, "could not set attributes");
         }
@@ -110,38 +172,6 @@ OpenGLMesh::OpenGLMesh(
     }
 
     unbind();
-}
-
-OpenGLMesh::~OpenGLMesh()
-{
-    ::glDeleteVertexArrays(1u, &vao_);
-}
-
-void OpenGLMesh::update_vertex_data(const std::vector<VertexData> &data)
-{
-    vertex_buffer_.write(data);
-}
-
-void OpenGLMesh::update_index_data(const std::vector<std::uint32_t> &data)
-{
-    index_buffer_.write(data);
-}
-
-GLsizei OpenGLMesh::element_count() const
-{
-    return static_cast<GLsizei>(index_buffer_.element_count());
-}
-
-void OpenGLMesh::bind() const
-{
-    ::glBindVertexArray(vao_);
-    expect(check_opengl_error, "could not bind vao");
-}
-
-void OpenGLMesh::unbind() const
-{
-    ::glBindVertexArray(0u);
-    expect(check_opengl_error, "could not unbind vao");
 }
 
 }
