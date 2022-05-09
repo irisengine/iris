9ea8836325493f4e0f4d3f874da5082df9fd2025
iris <iris-engine-dev@gmail.com>
2022-05-09 21:45:20 +0100

    Use MaterialCache in opengl renderer

--- include/iris/graphics/opengl/opengl_renderer.h
+++ include/iris/graphics/opengl/opengl_renderer.h
@@ -12,7 +12,7 @@
 #include <unordered_map>
 #include <vector>
 
-
+#include "graphics/material_cache.h"
 #include "graphics/opengl/opengl_buffer.h"
 #include "graphics/opengl/opengl_material.h"
 #include "graphics/opengl/opengl_render_target.h"
@@ -73,26 +73,13 @@ class OpenGLRenderer : public Renderer
     void execute_present(RenderCommand &command) override;
 
   private:
-    /**
-     * Custom equality functor for our unordered maps of render graphs. This is because we want no collisions, any graph
-     * hashing the same should be considered equal.
-     */
-    struct RenderGraphPtrEqual
-    {
-        bool operator()(const RenderGraph *a, const RenderGraph *b) const
-        {
-            return std::hash<RenderGraph *>{}(a) == std::hash<RenderGraph *>{}(b);
-        }
-    };
-
     // helper aliases to try and simplify the verbose types
     using LightMaterialMap = std::unordered_map<LightType, std::unique_ptr<OpenGLMaterial>>;
 
     /** Collection of created RenderTarget objects. */
     std::vector<std::unique_ptr<OpenGLRenderTarget>> render_targets_;
 
-    /** This collection stores materials per light type per render graph. */
-    std::unordered_map<RenderGraph *, LightMaterialMap, std::hash<RenderGraph *>, RenderGraphPtrEqual> materials_;
+    MaterialCache<OpenGLMaterial, RenderGraph *, LightType, bool, bool> materials_;
 
     /** Width of window being rendered to. */
     std::uint32_t width_;
