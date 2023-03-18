b07d4630e9d713138f54307be079c55e5ae90b98
iris-engine-dev <iris.engine.dev@gmail.com>
2023-03-18 13:00:41 +0000

    Initial skeleton for sclera

---
+++ tools/sclera/main.cpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+//         Distributed under the Boost Software License, Version 1.0.         //
+//            (See accompanying file LICENSE or copy at                       //
+//                 https://www.boost.org/LICENSE_1_0.txt)                     //
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world\n";
+
+    return 0;
+}
